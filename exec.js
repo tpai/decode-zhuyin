d2dccf015279a0ed0f2db251cddc5c91390ed084
tonypai <tony77794@gmail.com>
2017-07-17 12:12:17 +0800

    Import no-ch to replace duplicate code in exec.js

--- exec.js
+++ exec.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const decodeZhuyin = require('./');
+const decodeZhuyin = require('./no-ch');
 
 process.argv.splice(2).forEach(input => {
-    console.log(decodeZhuyin(input).replace(/[\,,\.,\;,\/,\-, ]/ig, ''));
+    console.log(decodeZhuyin(input));
 });
